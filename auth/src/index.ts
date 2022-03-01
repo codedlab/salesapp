f3950f89e910f9efb868fe672ae53330831471c4
Samuel Akrasi <sayakrasi@gmail.com>
2022-03-01 21:12:19 +0000

    updated all services

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log('app starting...');
+  console.log('app starting....');
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
